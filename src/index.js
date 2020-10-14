05598b33323a710e65bd2bbd1315275299bc29ee
Tanmay Verma <vermatanmay3@gmail.com>
2020-10-14 16:38:47 +0530

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 
 ReactDOM.render(
